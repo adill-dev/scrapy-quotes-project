9419f393b36b35c373da89dc3047d06e53bdaf7b
adill-dev <adil.saeed@saffranlabs.com>
2025-07-21 17:12:28 +0500

    scrapy quotes project

---
+++ tutorial/spiders/tag_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+class TagSpider(scrapy.Spider):
+    name = "tag"
+
+    async def start(self):
+        url = "https://quotes.toscrape.com/"
+        tag =  getattr(self, "tag", None)
+
+        if tag is not None:
+            url = url + 'tag/' + tag
+            yield scrapy.Request(url, callback=self.parse)
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text").get(),
+                "author": quote.css("small.author").get()
+            }
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)            
+
+     
