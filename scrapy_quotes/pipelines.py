7ed4c67052ff586a884e39d053b87daf9bbf1270
adill-dev <adil.saeed@saffranlabs.com>
2025-07-22 11:16:37 +0500

    move data to database

--- scrapy_quotes/pipelines.py
+++ scrapy_quotes/pipelines.py
@@ -5,9 +5,40 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+# from itemadapter import ItemAdapter
 
+import pymysql
+import os
+from dotenv import load_dotenv
+from datetime import datetime
 
-class TutorialPipeline:
+load_dotenv()
+
+class SaveAuthorPipeline:
+    def open_spider(self, spider):
+        self.connection = pymysql.connect(
+            host=os.getenv("DB_HOST"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database=os.getenv("DB_NAME"),
+            port=int(os.getenv("DB_PORT")),
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        self.cursor = self.connection.cursor()
+
+    def close_spider(self, spider):
+        self.connection.commit()
+        self.connection.close()
+
     def process_item(self, item, spider):
+        sql = """
+        INSERT INTO authors (name, dob, bio)
+        VALUES (%s,%s,%s)
+        """
+        dob_str = item['dob']
+        dob = datetime.strptime(dob_str, "%B %d, %Y").date()
+
+        self.cursor.execute(sql,(item['name'], dob, item['bio']))
+
         return item
--- scrapy_quotes/settings.py
+++ scrapy_quotes/settings.py
@@ -58,9 +58,9 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "tutorial.pipelines.TutorialPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "scrapy_quotes.pipelines.SaveAuthorPipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
